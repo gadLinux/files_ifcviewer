adf02ad01ccdbb283f8b2651d0267c65c86b58b5
ronnyrod <ronny.rodriguez@gmail.com>
2020-06-13 14:01:58 +0200

    new fields for flyTo features

--- js/annotation-binding-script.js
+++ js/annotation-binding-script.js
@@ -6,13 +6,32 @@ var sendMessage = function(msg) {
 var createAnnotation = function (fileId, annotation) {
    
 };
+/*
+            'eye_x' => $this->eyex,
+            'eye_y' => $this->eyey,
+            'eye_z' => $this->eyez,      
+            'up_x' => $this->upx,
+            'up_y' => $this->upy,
+            'up_z' => $this->upz,      
+            'look_x' => $this->lookx,
+            'look_y' => $this->looky,
+            'look_z' => $this->lookz,
+*/
 var updateAnnotation = function (fileId, annotation) {
 	if (annotation.viewer_id.startsWith("issue")) {
 		$.ajax({
 		    url: baseUrl + '/annotations/'+annotation.viewer_id.replace("issue_",""),
 		    type: 'PUT',
 		    contentType: 'application/json',
-		    data: JSON.stringify({ file_id: fileId, title: annotation.title, description: annotation.description, status: annotation.status, wx: annotation.wPos[0], wy: annotation.wPos[1], wz: annotation.wPos[2] })
+		    data: JSON.stringify({ 
+			    file_id: fileId, 
+			    title: annotation.title, 
+			    description: annotation.description, 
+			    status: annotation.status, 
+			    wx: annotation.wPos[0], 
+			    wy: annotation.wPos[1], 
+			    wz: annotation.wPos[2] 
+		    })
 		}).done(function (response) {
 		}).fail(function (response, code) {
 		    // handle failure
@@ -22,7 +41,23 @@ var updateAnnotation = function (fileId, annotation) {
 		    url: baseUrl + '/annotations',
 		    type: 'POST',
 		    contentType: 'application/json',
-		    data: JSON.stringify({ file_id: fileId, title: annotation.title, description: annotation.description, wx: annotation.wPos[0], wy: annotation.wPos[1], wz: annotation.wPos[2] })
+		    data: JSON.stringify({ 
+			    file_id: fileId, 
+			    title: annotation.title, 
+			    description: annotation.description, 
+			    wx: annotation.wPos[0], 
+			    wy: annotation.wPos[1], 
+			    wz: annotation.wPos[2],
+                            eye_x: annotation.eye[0],
+                            eye_y: annotation.eye[1],
+                            eye_z: annotation.eye[2],
+                            up_x: annotation.up[0],
+                            up_y: annotation.up[1],
+                            up_z: annotation.up[2],
+                            look_x: annotation.look[0],
+                            look_y: annotation.look[1],
+                            look_z: annotation.look[2] 
+		    })
 		}).done(function (response) {
 			var data = response;
 			data.viewer_id = annotation.viewer_id;
